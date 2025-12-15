fa66bda121f960144c507e7a53a97eae2dd214c4
Martín López <martin.g.lopez@gmail.com>
2025-12-15 11:30:59 -0300

    Created models DB but not migrated. Check liked_by and others for relationships appropiated structure

--- network/models.py
+++ network/models.py
@@ -4,3 +4,31 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name="posts")
+    body = models.TextField()
+    liked_by = models.ForeignKey(User,
+                                 on_delete=models.CASCADE,
+                                 related_name="likes",
+                                 null=True,
+                                 blank=True)
+    created_at = models.DateTimeField()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name="comments")
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name="comments_made")
+    body = models.TextField()
+    liked_by = models.ForeignKey(User,
+                                 on_delete=models.CASCADE,
+                                 null=True,
+                                 blank=True)
+    created_at = models.DateTimeField()
