4e9ed87c23bfb25c75df394d21d04d718ee730b7
Martín López <martin.g.lopez@gmail.com>
2025-12-22 11:14:13 -0300

    Basic style and HTML for post

---
+++ network/static/network/network.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Page sections button listeners
+    // [TODO]
+
+
+    // Load the lastest 10 posts
+    load_posts();
+
+});
+
+function load_posts(user) {
+
+    // if no user is sent, load all post
+}
