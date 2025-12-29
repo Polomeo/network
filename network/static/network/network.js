326b1482db5cd7cf69d40eeec53cde78c24ed022
Martín López <martin.g.lopez@gmail.com>
2025-12-29 09:31:05 -0300

    Embedded React code moved to static/network/network.js

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -4,11 +4,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     // Load the lastest 10 posts
-    load_posts();
+    // [TODO]
+    // load_posts();
 
 });
 
-function load_posts(user) {
-
-    // if no user is sent, load all post
+function PostView(props) {
+    return (
+        <div class="card mb-3">
+            <div class="card-body">
+                <h5 class="card-title">{props.username}</h5>
+                <a href="#">Edit post</a>
+                <h6 class="card-subtitle mb-2 text-muted">timestamp</h6>
+                <p class="card-text">{props.message}</p>
+                <a href="#">0 likes</a>
+            </div>
+        </div>
+    );
 }
+
+function App() {
+    return (
+        <div class="col-md-8">
+            <PostView username="Harry" message="Hello world! Going to fight a Dragon!" />
+            <PostView username="Ron" message="Viktor Krum is the best!" />
+            <PostView username="Hermione" message="Adivination is a very unreliable magic..." />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
