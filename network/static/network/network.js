2d8dfa6798c2372374e1fd0c2ec206f58648097b
Martín <martin.g.lopez@gmail.com>
2026-01-27 19:37:12 -0300

    Created profile page that only fetches specific user posts

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Navigation Buttons
+    try {
+        document.querySelector("#user-profile").addEventListener('click', () => load_profile(4));
+    }
+    catch(error) {
+        console.log("Error getting user-profile link. User not logged in.");
+        //console.error(error);
+    }
+    document.querySelector("#all-posts").addEventListener('click', () => load_all_posts);
 
     // Load all posts page
     load_all_posts();
@@ -9,38 +17,87 @@ document.addEventListener('DOMContentLoaded', function () {
 function load_all_posts()
 {
     const posts_view = document.querySelector('#posts-view');
-    show_page('#posts-view');
-
     posts_view.innerHTML = "<h3>All posts</h3>";
 
     // Fetch the posts
     fetch("posts/all", {cache: 'reload'})
     .then(response => response.json())
     .then(posts => {
-
+        
         // Log the posts [DEBUG]
         console.log(posts);
-
+        
         // Add each post to template
         posts.forEach(element => {
             
             // Create the elements
             const post_element = document.createElement('div');
             post_element.innerHTML = `<div class="card-body">
-                <h5 class="card-title">${element.author}</h5>
-                <a href="#">Edit post</a>
-                <h6 class="card-subtitle mb-3 text-muted">${element.created_at}</h6>
-                <p class="card-text">${element.body}</p>
-                <a href="#">{props.likes} 0 likes</a>
+            <h5 class="card-title">${element.author}</h5>
+            <a href="#">Edit post</a>
+            <h6 class="card-subtitle mb-3 text-muted">${element.created_at}</h6>
+            <p class="card-text">${element.body}</p>
+            <a href="#">{props.likes} 0 likes</a>
             </div>`;
             posts_view.append(post_element);
-
+            
             // Styling
             post_element.setAttribute('class', 'card mb-3');
-
-
+            
+            
         });
     });
+    
+    // Display the all posts page
+    show_page('#posts-view');
+}
+
+function load_profile(userId){
+    const posts_view = document.querySelector('#profile-view');
+    
+    
+    // Fetch the posts
+    fetch(`posts/${userId}`, {cache: 'reload'})
+    .then(response => response.json())
+    .then(posts => {
+        
+        // Log the posts [DEBUG]
+        console.log(posts);
+
+        // If there are no posts
+        if (posts.no_posts) {
+            const noPostMessage = document.createElement('h3');
+            noPostMessage.innerHTML = "This user has not posted yet."
+            posts_view.insertAdjacentElement('beforeend', noPostMessage);
+            return
+        }
+
+        const profile_title = document.createElement('h3');
+        profile_title.innerHTML = `<h3>${String(posts[0].author).charAt(0).toUpperCase() + String(posts[0].author).slice(1)}'s posts</h3>`;
+        posts_view.append(profile_title);
+        
+        // Add each post to template
+        posts.forEach(element => {
+            
+            // Create the elements
+            const post_element = document.createElement('div');
+            post_element.innerHTML = `<div class="card-body">
+            <h5 class="card-title">${element.author}</h5>
+            <a href="#">Edit post</a>
+            <h6 class="card-subtitle mb-3 text-muted">${element.created_at}</h6>
+            <p class="card-text">${element.body}</p>
+            <a href="#">{props.likes} 0 likes</a>
+            </div>`;
+            posts_view.append(post_element);
+            
+            // Styling
+            post_element.setAttribute('class', 'card mb-3');
+            
+        });
+    }); 
+
+    // Display profile page
+    show_page('#profile-view');
 }
 
 // Utilitary functions
--- network/views.py
+++ network/views.py
@@ -80,7 +80,7 @@ def load_user_posts(request, user_id):
     posts = Post.objects.filter(author_id = user_id)
     
     if len(posts) == 0:
-        return JsonResponse({"no-posts": "There are no post yet."}, status=200)
+        return JsonResponse({"no_posts": "There are no post yet."}, status=200)
     else:
         posts = posts.order_by("-created_at").all()
         return JsonResponse([post.serialize() for post in posts], safe=False)
