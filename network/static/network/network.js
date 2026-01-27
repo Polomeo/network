1158d2332e00af76cd07b982fc795fd9f837d4da
Martín <martin.g.lopez@gmail.com>
2026-01-27 18:53:58 -0300

    Showing posts correctly with Vanilla Javascript

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function load_all_posts()
 {
     const posts_view = document.querySelector('#posts-view');
+    show_page('#posts-view');
 
     posts_view.innerHTML = "<h3>All posts</h3>";
 
@@ -16,7 +17,29 @@ function load_all_posts()
     fetch("posts/all", {cache: 'reload'})
     .then(response => response.json())
     .then(posts => {
+
+        // Log the posts [DEBUG]
         console.log(posts);
+
+        // Add each post to template
+        posts.forEach(element => {
+            
+            // Create the elements
+            const post_element = document.createElement('div');
+            post_element.innerHTML = `<div class="card-body">
+                <h5 class="card-title">${element.author}</h5>
+                <a href="#">Edit post</a>
+                <h6 class="card-subtitle mb-3 text-muted">${element.created_at}</h6>
+                <p class="card-text">${element.body}</p>
+                <a href="#">{props.likes} 0 likes</a>
+            </div>`;
+            posts_view.append(post_element);
+
+            // Styling
+            post_element.setAttribute('class', 'card mb-3');
+
+
+        });
     });
 }
 
