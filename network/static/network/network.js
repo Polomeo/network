aa8fe09b8b01c60361002e2c99438d0553a9288a
Martín López <martin.g.lopez@gmail.com>
2026-03-05 12:11:43 -0300

    Bug found regarding follow link in user profile avatar not redirecting to login page

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -179,24 +179,24 @@ function loadProfile(userId) {
 
 // Follows the current user
 function followUser(profileUserId) {
-       
+
     fetch(`posts/follow/${profileUserId}`, {
         method: 'POST',
     })
-    .then(response => response.json())
-    .then(result => {
-        if(result.followed){
-            console.log("User is now following.")
-            updateFollowers(profileUserId);
-        }
-        else if (result.unfollowed){
-            console.log("User is no longer following.")
-            updateFollowers(profileUserId);
-        }
-    })
-    .catch(error => {
-        console.log('Error: ', error);
-    });
+        .then(response => response.json())
+        .then(result => {
+            if (result.followed) {
+                console.log("User is now following.")
+                updateFollowers(profileUserId);
+            }
+            else if (result.unfollowed) {
+                console.log("User is no longer following.")
+                updateFollowers(profileUserId);
+            }
+        })
+        .catch(error => {
+            console.log('Error: ', error);
+        });
 
 }
 
@@ -222,7 +222,7 @@ function createAvatar(username, profileUserId) {
     return profileAvatar
 }
 
-function updateFollowers(profileUserId){
+function updateFollowers(profileUserId) {
     // Updates all follower related visuals and checks
 
     let currentUserId = 0
@@ -238,7 +238,7 @@ function updateFollowers(profileUserId){
     }
 
     // Fetch the profile followers
-    fetch(`posts/followers/${profileUserId}`, {cache: 'reload'})
+    fetch(`posts/followers/${profileUserId}`, { cache: 'reload' })
         .then(response => response.json())
         .then(followers => {
             if (followers.no_followers) {
@@ -248,7 +248,7 @@ function updateFollowers(profileUserId){
                 followers.forEach(element => {
                     followerCount++;
                     console.log("Follower count: " + String(followerCount));
-                    
+
                     // Check if the current user already follows this profile
                     if (element['follower_id'] == currentUserId) {
                         userIsFollowing = true;
@@ -259,25 +259,25 @@ function updateFollowers(profileUserId){
             // Set the followers number
             console.log("Setting followers number...");
             document.querySelector("#followers").innerHTML = String(followerCount);
-            
+
             // Set the follow button
             console.log("Setting follow button...");
-            document.querySelector("#follow-link").innerHTML = userIsFollowing? "Unfollow" : "Follow";
+            document.querySelector("#follow-link").innerHTML = userIsFollowing ? "Unfollow" : "Follow";
         });
 }
 
-function updateFollowings(profileUserId){
+function updateFollowings(profileUserId) {
 
     followingCount = 0;
 
-    fetch(`posts/following/${profileUserId}`, {cache : 'reload'})
+    fetch(`posts/following/${profileUserId}`, { cache: 'reload' })
         .then(response => response.json())
         .then(following => {
-            if (following.no_followings){
+            if (following.no_followings) {
                 console.log(following.no_followings);
             }
             else {
-                following.forEach(element =>{
+                following.forEach(element => {
                     followingCount++;
                     console.log(element);
                 })
